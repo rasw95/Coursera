98ccb22e2418077787bda9a144eb5891d7e34d14
sami <samiramoul95@yahoo.fr>
2023-01-10 15:20:29 -0500

    Setup MYSQL DB and Created models

--- Meta_Back-end_Dev/Littlelemon/Littlelemon/settings.py
+++ Meta_Back-end_Dev/Littlelemon/Littlelemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant', # Add thr APP.
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'Littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restaurant',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'sami',
+        'PASSWORD': 'sami1234',
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  # Handle invalid or mission values when storiung data (Insert and Update).
+        }  
     }
 }
 
--- Meta_Back-end_Dev/Littlelemon/restaurant/models.py
+++ Meta_Back-end_Dev/Littlelemon/restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class booking(models.Model):
+    Name = models.CharField(max_length=255, null=False, blank=False)
+    No_of_guests = models.IntegerField(default=1, validators=[MaxValueValidator(6),MinValueValidator(1)])
+    BookingDate = models.DateTimeField(null=False)
+    def __str__(self) -> str:
+        return self.Name
+
+class menu(models.Model):
+    Title = models.CharField(max_length=255, null=False, blank=False)
+    Price =models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.SmallIntegerField(default= 0,  validators=[MaxValueValidator(10), MinValueValidator(1)]) 
+    def __str__(self) -> str:
+        return self.Title
+
+
