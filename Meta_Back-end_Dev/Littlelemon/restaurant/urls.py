94c304a44a1d34b2ca15750c19fcb4fbbafac55d
sami <samiramoul95@yahoo.fr>
2023-01-19 11:06:55 -0500

    Bug Fix (url file: Book class not found)

--- Meta_Back-end_Dev/Littlelemon/restaurant/urls.py
+++ Meta_Back-end_Dev/Littlelemon/restaurant/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
-    path('book/', views.book, name="book"),
+    #path('book/', views.book, name="book"),
     # Add the remaining URL path configurations here
     path('menu/', views.menu, name = 'menu'),
     path('menu_item/<int:pk>/', views.display_items, name= 'menu_item'), 
